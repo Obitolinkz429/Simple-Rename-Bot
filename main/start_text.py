1cd86aa41852793f8a0cb06704a9afaadf8c4653
Obitolinkz429 <126272987+Obitolinkz429@users.noreply.github.com>
2023-04-07 20:45:49 +0530

    Update start_text.py

--- main/start_text.py
+++ main/start_text.py
@@ -7,7 +7,7 @@ from config import ADMIN
 async def start_cmd(bot, msg):
     txt="This is personal use bot 🙏. Do you want your own bot? 👇 Click the source code to deploy"
     btn = InlineKeyboardMarkup([[
-        InlineKeyboardButton("🤖 SOURCE CODE", url="https://github.com/MrMKN/Simple-Rename-Bot")
+        InlineKeyboardButton("🤖 SOURCE CODE", url="https://github.com/Obitolinkz429/Simple-Rename-Bot")
         ],[
         InlineKeyboardButton("🖥️ How To Deploy", url="https://youtu.be/oc847WvOUaI")
     ]])
@@ -20,7 +20,7 @@ async def start_cmd(bot, msg):
 async def start(bot, msg, cb=True):   
     txt=f"hai {msg.from_user.mention} i am simple rename bot with personal usage.\nthis bot is made by <b><a href=https://github.com/MrMKN>MrMKN</a></b>"                                     
     button= [[
-        InlineKeyboardButton("🤖 Bot Updates", url="https://t.me/mkn_bots_updates")
+        InlineKeyboardButton("🤖 Bot Updates", url="https://t.me/teamnetfilx2")
         ],[
         InlineKeyboardButton("ℹ️ Help", callback_data="help"),
         InlineKeyboardButton("📡 About", callback_data="about") 
